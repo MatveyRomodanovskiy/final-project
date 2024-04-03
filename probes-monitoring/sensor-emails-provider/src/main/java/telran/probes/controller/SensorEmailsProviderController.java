ba937ed86a1bd398d8d924ffd96f4b648d2b0b1b
MatveyRomodanovskiy <startpolus@gmail.com>
2024-04-04 01:59:00 +0300

    Stage2 - add Emails Provider

--- probes-monitoring/sensor-emails-provider/src/main/java/telran/probes/controller/SensorEmailsProviderController.java
+++ probes-monitoring/sensor-emails-provider/src/main/java/telran/probes/controller/SensorEmailsProviderController.java
@@ -1,21 +1,20 @@
 package telran.probes.controller;
+import java.util.Arrays;
 
 import org.springframework.web.bind.annotation.*;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import telran.probes.service.SensorEmailsProviderService;
-import static telran.probes.UrlConstants.*;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class SensorEmailsProviderController {
-	final SensorEmailsProviderService service;
-	@GetMapping(EMAILS_PATH + "{id}")
-	String[] getSensorEmails(@PathVariable(name="id") long id) {
-		String[] sensorEmails =  service.getSensorEmails(id);
-		log.debug("sensor list Emails size received is {}", sensorEmails.length);
-		return sensorEmails;
-	}
-	
-	
+final SensorEmailsProviderService providerService;
+@GetMapping("${app.emails.provider.url}" + "/{id}")
+String[] getEmails(@PathVariable(name="id") long id) {
+	String[] emails =  providerService.getSensorEmails(id);
+	log.debug("emails received are {}", Arrays.deepToString(emails));
+	return emails;
+}
 }
